b708539c2b394ca47fd7413e5763b6b92de4c122
david-lambropoulos-91 <d.a.lambropoulos91@gmail.com>
2015-01-03 06:17:49 -0500

    Create histogram.c

---
+++ histogram.c
@@ -0,0 +1,67 @@
+/**
+*	@author: David Lambropoulos
+*	Date: 12/28/2014
+*	
+*	Exercise 1-13: The C Programming Language 
+*			Ritchie and Kernighan
+*
+*	Write a program to print a histogram of the lengths
+*	of words in its input using vertical orientation.
+**/
+
+#include <stdio.h>
+
+main()
+{
+	int c, wc, i, j, largest;
+	int histogram[10];
+
+	wc = 0;
+
+	for(i = 0; i < 10; ++i)
+		histogram[i] = 0;
+
+	while ((c = getchar()) != EOF)
+	{
+		if (c == ' ' || c == '\n' || c == '\t')
+		{
+
+			histogram[wc] = histogram[wc] + 1;
+			wc = 0;
+		}
+		else
+		{
+			 wc++;
+		}
+	}
+
+	printf("\n");
+
+	for (i = 0; i < 10; ++i)
+		if (i == 0)
+			largest = histogram[i];
+		else if (histogram[i] > largest)
+			largest = histogram[i];
+
+	for(i = largest; i > 0; --i)
+	{
+		printf("%i |", i);
+		for(j = 0; j < 10; ++j)
+		{
+			if (histogram[j] == i)
+			{
+				printf("x");
+				histogram[j] = histogram[j] - 1;
+			}
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
+
+	printf("-------------\n   ");
+
+	for (i = 0; i < 10; ++i)
+		printf("%i",i);
+	printf("\n");
+}
