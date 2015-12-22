2779ea652861bc609e6e162e907d8ee1ce428d69
David Lambropoulos <d.a.lambropoulos91@gmail.com>
2015-12-22 01:11:36 -0500

    Create alerts.h

---
+++ Indexer/alerts.h
@@ -0,0 +1,25 @@
+#ifndef ALERT_H
+#define ALERT_H
+
+/*
+ *	alerts.h
+ */
+
+
+void warning(char * msg);
+
+void error(char * msg);
+
+void success(char * msg);
+
+void genwarn();
+
+void generr();
+
+void gensucc();
+
+
+
+
+
+#endif
